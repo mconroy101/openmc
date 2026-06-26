ca53f0326ec2b17bb841a35d358269e0ee6a9ba3
Max Conroy <mjc970@student.bham.ac.uk>
2026-06-26 14:55:27 +0100

    Actually fix SourceSite bug

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -177,8 +177,7 @@ void initialize_mpi(MPI_Comm intracomm)
   MPI_Get_address(&b.wgt_ww_born, &disp[12]);
   MPI_Get_address(&b.n_split, &disp[13]);
   MPI__Get_address(&b.parent_type, &disp[14]);
-  for (int i = 13; i >= 0; --i) {
-  for (int i = 11; i >= 0; --i) {
+  for (int i = 14; i >= 0; --i) {
     disp[i] -= disp[0];
   }
 
