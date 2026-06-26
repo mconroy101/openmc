98403a1a9b1340d132b80e55fa1f74aca8ddb381
Max Conroy <mjc970@student.bham.ac.uk>
2026-06-26 14:50:47 +0100

    Fix SourceSiite parent_type

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -176,8 +176,8 @@ void initialize_mpi(MPI_Comm intracomm)
   MPI_Get_address(&b.wgt_born, &disp[11]);
   MPI_Get_address(&b.wgt_ww_born, &disp[12]);
   MPI_Get_address(&b.n_split, &disp[13]);
+  MPI__Get_address(&b.parent_type, &disp[14]);
   for (int i = 13; i >= 0; --i) {
-  MPI__Get_address(&b.parent_type, &disp[11]);
   for (int i = 11; i >= 0; --i) {
     disp[i] -= disp[0];
   }
